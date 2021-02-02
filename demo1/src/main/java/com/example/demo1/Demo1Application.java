429d84629b819b8cfd12b2b9ee03a12365fa6e68
chichi <djimgoupat@yahoo.fr>
2021-02-02 19:05:08 +0100

    upload first Step Spring boot

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -1,15 +1,13 @@
-package com.firststepspringboot.firststep;
+package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirststepApplication {
+public class Demo1Application {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(FirststepApplication.class, args);
-
+        SpringApplication.run(Demo1Application.class, args);
     }
 
 }
